4e8e9847f1921b9961b1d1dc6f35d2ed0bcafd60
Bolun <Bolun.Han@outlook.com>
2024-05-28 18:42:14 +0800

    added a new performance test

--- Demo/performance.py
+++ Demo/performance.py
@@ -1,6 +1,6 @@
 import time
 
-from EventEngine import Topic, EventEngine, LOGGER
+from EventEngine import Topic, EventEngine, LOGGER, PatternTopic
 
 BUFFER_SIZE = 1024
 EVENT_ENGINE = EventEngine(buffer_size=BUFFER_SIZE)
@@ -23,7 +23,21 @@ def init_test():
     LOGGER.info(f'All {N:,d} task done, time cost {time.time() - ts:.2f}s.')
 
 
+def init_test_pattern():
+    ts = time.time()
+    pattern_topic = PatternTopic('realtime.{ticker}.{dtype}')
+    topic = Topic('realtime.APPL.TradeData')
+    data = {'dtype': 'TradeData', 'ticker': 'APPL', 'price': 95., 'volume': 200}
+    EVENT_ENGINE.register_handler(topic=pattern_topic, handler=on_data)
+
+    for _ in range(N):
+        EVENT_ENGINE.put(topic=topic, data=data)
+
+    LOGGER.info(f'All {N:,d} task done, time cost {time.time() - ts:.2f}s.')
+
+
 if __name__ == '__main__':
     EVENT_ENGINE.start()
     init_test()
+    # init_test_pattern()
     EVENT_ENGINE.stop()

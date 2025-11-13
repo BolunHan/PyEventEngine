ea1dae7afba494ae3534eb724be5556f655179b8
Han Bolun <Bolun.Han@outlook.com>
2025-11-13 20:35:21 +0800

    added get_include

--- event_engine/__init__.py
+++ event_engine/__init__.py
@@ -1,8 +1,18 @@
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
+import functools
 import os
 
 if os.name == 'posix':
     from .capi import *
 else:
     from .native import *
+
+
+@functools.cache
+def get_include():
+    import os
+
+    res_dir = os.path.dirname(__file__)
+    LOGGER.info(f'Building with <PyEventEngine> version: "{__version__}", resource directory: "{res_dir}".')
+    return res_dir

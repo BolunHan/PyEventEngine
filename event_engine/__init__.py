6407a15f6718d41fc1df41d9811cda9b230d64b6
Han Bolun <Bolun.Han@outlook.com>
2025-12-23 19:36:53 +0800

    get_include returns all the dirs instead of root dir.

--- event_engine/__init__.py
+++ event_engine/__init__.py
@@ -1,11 +1,13 @@
 __version__ = '0.4.5'
 
 import functools
+import pathlib
 
 try:
     from .capi import *  # noqa: F401,F403
 except Exception as e:
     import warnings
+
     warnings.warn(
         f"Failed to import event_engine.capi ({e!r}); falling back to event_engine.native.",
         ImportWarning,
@@ -15,10 +17,10 @@ except Exception as e:
 
 
 @functools.cache
-def get_include():
+def get_include() -> list[str]:
     import os
     from .base import LOGGER
 
     res_dir = os.path.dirname(__file__)
     LOGGER.info(f'Building with <PyEventEngine> version: "{__version__}", resource directory: "{res_dir}".')
-    return res_dir
+    return [res_dir, pathlib.Path(res_dir).joinpath('base').__str__(), pathlib.Path(res_dir).joinpath('capi').__str__()]
